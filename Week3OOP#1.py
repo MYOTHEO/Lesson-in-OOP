370e8a21e1c8ebb24edb3d4b56447e86ced17c6b
MYOTHEO <93719723+MYOTHEO@users.noreply.github.com>
2024-09-09 16:49:58 +0800

    Delete Week3OOP#1.py

--- Week3OOP#1.py
+++
@@ -1,8 +0,0 @@
-class character:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-        
-student = character('Meynard', 19)
-student2 = character('Sean', 20)
-print(student.name, student.age, student2.name, student2.age)
