f3fa4a7bbc001035ab1e2dbbf1b4e95c44d4e54d
MYOTHEO <93719723+MYOTHEO@users.noreply.github.com>
2024-09-09 16:37:36 +0800

    Add files via upload

---
+++ Week3OOP.py
@@ -0,0 +1,19 @@
+class hero():
+    name = "Argus"
+    name2 = "nigro"
+    hp = 100
+    basic_attack = 10
+fighter = hero() # object
+support = hero() # object
+
+print(fighter.name)
+print(support.name)
+
+
+
+
+#   ----------NOTES----------
+'''
+class - reserved keyword
+attribute - 
+''' 
