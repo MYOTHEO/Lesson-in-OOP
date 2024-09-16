fbbd2bac8f4550f63cdb0f456aeb62690ab79ac1
MYOTHEO <93719723+MYOTHEO@users.noreply.github.com>
2024-09-16 16:32:10 +0800

    Add files via upload

---
+++ Week 5 - INHERITANCE.py
@@ -0,0 +1,13 @@
+class Hero():
+    def __init__(self, name, health):
+        self.name = name
+        self.health = health
+    
+    def describeHero(self):
+            
+    
+class Marksman(Hero):
+    pass
+
+miya = Marksman("Miya","200")
+miya.describeHero()
