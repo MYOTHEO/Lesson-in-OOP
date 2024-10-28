ac84a98e187c65988e0a4d5b1fda3b607a6ebc1d
MYOTHEO <93719723+MYOTHEO@users.noreply.github.com>
2024-10-28 14:10:59 +0800

    Week 9 ENCAPSULATION SETTER.py

--- Week 9 ENCAPSULATION SETTER.py
+++ Week 9 ENCAPSULATION SETTER.py
@@ -1,12 +1,21 @@
-#Encapsulation
+#Encapsulation Getter
 class Lumpia_wrapper:
     def __init__(self, meat, onion, carrot):
-        self.__meat = meat
-        self.onion = onion
-        self.carrot = carrot
+        self.meat = meat            #public
+        self._onion = onion          #protected
+        self.__carrot = carrot      #private
         
-        def __str__(self):
-            return f"Ang lumpia ko ay may {self.__meat}, {self.onion}, {self.carrot}"
-
+    def __str__(self):
+        return f"Ang lumpia ko ay may {self.meat}, {self._onion}, {self.__carrot}"
+    def may_carrots_ba(self): #getter method
+            return self.__carrot
+    def set_carrot(self, bagong_value): #setter method
+        self.__carrot = bagong_value
+            
 lumpiang_shanghai = Lumpia_wrapper("baboy", "isang sibuyas", "isang carrot")
-print(lumpiang_shanghai.__meat)
+lumpiang_shanghai.set_carrot = ("limang carrot")
+lumpiang_shanghai.__carrot = "dalawang carrot"
+print(lumpiang_shanghai.__carrot)
+print(lumpiang_shanghai.set_carrot)
+# (__) private is accessible 
+# (_) protected is still accessible
